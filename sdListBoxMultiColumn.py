957cea5e74647181411f42998349edc9144b8a30
Sandeep Shampur <50356291+sandeepshampur@users.noreply.github.com>
2024-08-02 15:29:35 +0530

    Major enhancements
    1. Added additional parameters to "__init__()" function (colours / font)
    2. Modified "Insert()" to accept position to insert
    3. Added function "Move()"

--- sdListBoxMultiColumn.py
+++ sdListBoxMultiColumn.py
@@ -9,10 +9,13 @@
 #							  5. Added function "Bind()"
 # Enhancement : 14-Mar-2022 : Added function "DeleteRow()"
 # Enhancement : 21-Jun-2024 : Added function "_sort_by_multidecimal" in clListBoxMultiColumnSort
-#
 # Fix		  : 22-Jun-2024 : Added check in clListBoxMultiColumn->FormatDate() to leave date untouched if it is already having hyphens ("-")
 # Enhancement : 30-Jun-2024 : Added flag "bSortDescending" to choose order of sorting
-# Enhancement : 17-Jul-2024 : Added function "SetSelection()" to set a specific item
+# Enhancement : 18-Jul-2024 : Added function "SelectRow()" to select a specific row
+# Enhancement : 02-Aug-2024 : 1. Added additional parameters to "__init__()" function (colours / font)
+#							  2. Modified "Insert()" to accept position to insert
+#							  3. Added function "Move()"
+#
 
 import datetime as objDateTime
 from functools import partial
@@ -21,39 +24,50 @@ import tkinter as objTK
 from tkinter import ttk as objTTK
 
 class clListBoxMultiColumn:
-	def __init__(self, dictTvHdrAttrib, arrDefaultRecords=[], iSortByColumn=-1, colourFg="black", colourBg="white"):
+	def __init__(self, dictTvHdrAttrib, arrDefaultRecords=[], iSortByColumn=-1, colourFg="black", colourBg="#d9d9d9",
+				 colourHdr="#C3C3C3", tagBgEven="#f5f5f5", tagBgOdd="#dcdcff", font="Arial 11 normal", objLoggerLog=None):
 		self.dictTvHdrAttrib = dictTvHdrAttrib
 		self.arrDefaultRecords = arrDefaultRecords
 		self.iColumnToSort = iSortByColumn
 		self.colourFg = colourFg
 		self.colourBg = colourBg
+		self.colourHdr = colourHdr
+		self.tagBgEven = tagBgEven
+		self.tagBgOdd = tagBgOdd
+		self.strFont = font
+		self.arrFont = self.strFont.split(" ")
+		self.objLoggerLog = objLoggerLog
 
 		# Identify columns with date for formatting later
 		self.arrDtCol = []
 		iIndex = 0
 		arrHdr = self.dictTvHdrAttrib["sortType"]
 		if "date" in arrHdr:
-			for x in range(1000):
+			while(1):
 				try:
 					iIndex = arrHdr.index("date", iIndex)
-					self.arrDtCol.append(iIndex)
-					iIndex += 1
 				except:
 					break
 				# End of try / except
-			# End of for loop
+
+				self.arrDtCol.append(iIndex)
+				iIndex += 1
+			# End of while loop
 		# End of if
 	# End of __init__()
 	
 	def Display(self, objWindow, strSelectMode, ifrX, ifrY, ifrW, ifrH, arrRecords, bSortDescending=False, strHeader=""):
-		objFrame = objTK.LabelFrame(objWindow, text=strHeader, foreground=self.colourFg, background=self.colourBg)
+		objFrame = objTK.LabelFrame(objWindow, text=strHeader, foreground=self.colourFg, background=self.colourBg, font=self.strFont)
 		self.bSortDescending = bSortDescending
 
 		# Set header background colour
 		objStyle = objTTK.Style()
-		objStyle.configure("tvResult.Treeview.Heading", background="#C3C3C3")
+		objStyle.configure("tvResult.Treeview", background="black", fieldbackground=self.tagBgEven, font=(self.strFont))
+		objStyle.configure("tvResult.Treeview.Heading", background=self.colourHdr, font=(self.arrFont[0], self.arrFont[1], "bold"))
 
-		self.tvRecords = clListBoxMultiColumnSort(master=objFrame, columns=self.dictTvHdrAttrib["header"], show="headings", selectmode=strSelectMode, style="tvResult.Treeview", bSortDescending=self.bSortDescending)
+		self.tvRecords = clListBoxMultiColumnSort(master=objFrame, columns=self.dictTvHdrAttrib["header"], show="headings", selectmode=strSelectMode,
+												  style="tvResult.Treeview", tagBgEven=self.tagBgEven, tagBgOdd=self.tagBgOdd,
+												  bSortDescending=self.bSortDescending, objLoggerLog=self.objLoggerLog)
 		objRWHScr = objTTK.Scrollbar(master=objFrame, orient="horizontal", command=self.tvRecords.xview)
 		objRWHScr.grid(column=0, row=1, sticky="ew", in_=objFrame)		
 		objRWVScr = objTTK.Scrollbar(master=objFrame, orient="vertical", command=self.tvRecords.yview)
@@ -72,7 +86,8 @@ class clListBoxMultiColumn:
 			iHdrW = self.dictTvHdrAttrib["width"][iCount]
 
 			self.tvRecords.heading(strHdr, text=strHdr, sort_by=self.dictTvHdrAttrib["sortType"][iCount])
-			self.tvRecords.column(self.dictTvHdrAttrib["header"][iCount], minwidth=iHdrW, width=iHdrW, stretch=False, anchor=self.dictTvHdrAttrib["alignment"][iCount])
+			self.tvRecords.column(self.dictTvHdrAttrib["header"][iCount], minwidth=iHdrW, width=iHdrW, stretch=False,
+								  anchor=self.dictTvHdrAttrib["alignment"][iCount])
 		# End of for loop
 		
 		# Insert data
@@ -93,11 +108,24 @@ class clListBoxMultiColumn:
 	# End of Bind()
 	
 	def ClearAll(self):
-		self.tvRecords.delete(*self.tvRecords.get_children())
+		self.tvRecords.delete(*self.tvRecords.get_children(""))
 	# End of ClearAll()
 
 	def DeleteRow(self, iRow):
-		self.tvRecords.delete(self.tvRecords.get_children()[iRow])
+		# iRow is zero-based
+
+		for x in range(1):
+			arrIIDs = self.tvRecords.get_children("")
+			if len(arrIIDs) == 0:
+				break
+			# End of if
+
+			if (iRow+1) > len(arrIIDs):
+				break
+			# End of if
+
+			self.tvRecords.delete(arrIIDs[iRow])
+		# End of for loop
 	# End of DeleteRow()
 	
 	def DeleteSelection(self):
@@ -117,14 +145,14 @@ class clListBoxMultiColumn:
 	# End of Enable()
 	
 	def GetCount(self):
-		arrItems = self.tvRecords.get_children()
-		return len(arrItems)
+		arrIIDs = self.tvRecords.get_children("")
+		return len(arrIIDs)
 	# End of GetCount()
 
 	def GetAllEntries(self):
 		arrResult = []
-		arrItems = self.tvRecords.get_children()
-		for objItem in arrItems:
+		arrIIDs = self.tvRecords.get_children("")
+		for objItem in arrIIDs:
 			arrResult.append(self.tvRecords.item(objItem)["values"])
 			
 			# Format date if present
@@ -135,9 +163,9 @@ class clListBoxMultiColumn:
 	# End of GetAllEntries()
 	
 	def GetSelectionEntries(self):		
-		arrResult = []		
-		arrItems = self.tvRecords.selection()
-		for objItem in arrItems:
+		arrResult = []
+		arrIIDs = self.tvRecords.selection()
+		for objItem in arrIIDs:
 			arrResult.append(self.tvRecords.item(objItem)["values"])
 			
 			self.UnFormatDate(arrResult[-1])
@@ -145,37 +173,112 @@ class clListBoxMultiColumn:
 		
 		return arrResult
 	# End of GetSelectionEntries()
-	
-	def Insert(self, arrList):
-		# Add to listbox
-		iRows = self.GetCount()
-		for iIndex in range(len(arrList)):
-			arrRow = arrList[iIndex]
-			
+
+	def GetSelectionRows(self):
+		arrRows = []
+		arrIIDs = self.tvRecords.selection()
+		arrListIIDs = self.tvRecords.get_children("")
+		for strID in arrIIDs:
+			arrRows.append(arrListIIDs.index(strID))
+		# End of for loop
+
+		return arrRows
+	# End of GetSelectionRows()
+
+	def Insert(self, arrList, strPosition="end"):
+		# strPosition is zero-based
+
+		# Initialise
+		iTotalRows = self.GetCount()
+		if isinstance(strPosition, str):
+			iInsertPosition = iTotalRows
+		else:
+			iInsertPosition = int(strPosition)
+		# End of if
+
+		if iInsertPosition < 0:
+			iInsertPosition = 0
+		elif iInsertPosition > iTotalRows:
+			iInsertPosition = iTotalRows
+		# End of if
+
+		# Insert values
+		for arrRow in arrList:
 			self.FormatDate(arrRow)
 		
-			if iRows % 2 == 0:
-				self.tvRecords.insert("", "end", values=arrList[iIndex], tags="tgEvenRow")
+			if iInsertPosition % 2 == 0:
+				self.tvRecords.insert("", iInsertPosition, None, values=arrRow, tags="tgEvenRow")
 			else:
-				self.tvRecords.insert("", "end", values=arrList[iIndex], tags="tgOddRow")
+				self.tvRecords.insert("", iInsertPosition, None, values=arrRow, tags="tgOddRow")
 			# End of if
-			iRows += 1
+
+			iInsertPosition += 1
+			iTotalRows += 1
 		# End of for loop
-		
+
+		# Retag till end of listbox
+		arrIIDs = self.tvRecords.get_children("")
+		while iInsertPosition < iTotalRows:
+			if iInsertPosition % 2 == 0:
+				self.tvRecords.item(arrIIDs[iInsertPosition], tags="tgEvenRow")
+			else:
+				self.tvRecords.item(arrIIDs[iInsertPosition], tags="tgOddRow")
+			# End of if
+			iInsertPosition += 1
+		# End of while loop
+
 		if self.iColumnToSort != -1:
 			self.Sort(self.dictTvHdrAttrib["sortType"][self.iColumnToSort], self.dictTvHdrAttrib["header"][self.iColumnToSort], self.bSortDescending)
 		# End of if
 	# End of Insert()
+
+	def Move(self, strDirection):
+		# strDirection "Up" or "Down"
+
+		for x in range(1):
+			# Get selected row details
+			iCurrentPosition = self.GetSelectionRows()[0]
+			arrCurrentIID = self.tvRecords.selection()
+			iTotalRows = self.GetCount()
+
+			# Ignore end cases
+			if (strDirection.find("Up") == 0) and (iCurrentPosition == 0):
+				break
+			# End of if
+			if (strDirection.find("Down") == 0) and (iCurrentPosition == (iTotalRows-1)):
+				break
+			# End of if
+
+			# Move row
+			if strDirection.find("Up") == 0:
+				self.tvRecords.move(arrCurrentIID[0], '', iCurrentPosition-1)
+				iCurrentPosition -= 1
+			else:
+				self.tvRecords.move(arrCurrentIID[0], '', iCurrentPosition+1)
+			# End of if
+
+			# Retag rows
+			arrIIDs = self.tvRecords.get_children("")
+			while iCurrentPosition < iTotalRows:
+				if iCurrentPosition % 2 == 0:
+					self.tvRecords.item(arrIIDs[iCurrentPosition], tags="tgEvenRow")
+				else:
+					self.tvRecords.item(arrIIDs[iCurrentPosition], tags="tgOddRow")
+				# End of if
+				iCurrentPosition += 1
+			# End of while loop
+		# End of for loop
+	# End of GetSelectionIndex()
 
 	def Reset(self):
 		self.ClearAll()
 		self.Insert(self.arrDefaultRecords)
 	# End of Reset()
 
-	def SetSelection(self, iRowPosition):
+	def SelectRow(self, iRowPosition):
 		for x in range(1):
 			# Get records
-			arrList = self.tvRecords.get_children()
+			arrList = self.tvRecords.get_children("")
 			try:
 				self.tvRecords.selection_set(arrList[iRowPosition])
 			except:
@@ -222,23 +325,46 @@ class clListBoxMultiColumn:
 # End of class clListBoxMultiColumn
 
 class clListBoxMultiColumnSort(objTTK.Treeview):
-	# Sorting customised from 
-	# https://stackoverflow.com/questions/1966929/tk-treeview-column-sort/69294086#69294086
+	'''
+	Sorting customised from
+	https://stackoverflow.com/questions/1966929/tk-treeview-column-sort/69294086#69294086
+
+	Sorting types allowed
+		Particulars	:	Key				: Description
+	===========================================================================================
+	1. Date			:	"date"			: Date with zero padding e.g.: 1973-10-03
+	2. Float		:	"float"			: Float number e.g.: 12.0
+	3. Multidecimal	:	"multidecimal" 	: Numbers with multiple periods e.g.: 12.5.7.12
+	4. Number		:	"num"			: Integers
+	5. Number		:	"numcomma"		: Numbers with comma e.g.: 12,000
+	6. Name			:	"name"			: Text
+	7. Time			:	"time"			: Time in 12 format with zero padding e.g.: 11:03PM
+
+	Sorting type defaults to "name" for invalid key
+	LIMITATION of "multidecimal":
+	 			1, works only for two digits or less. It does not work for more than three digits (e.g.: 3.111.1).
+	 			2. The maximum decimals allowed is eight. (e.g.: 99.99.99.99.99.99.99.99.99). Any excess is truncated
+	'''
 	
-	def __init__(self, master=None, my_list=[], bSortDescending=False, tagBgEven="#FFFFFF", tagBgOdd="#E8E8FF", **kwargs):
+	def __init__(self, master=None, my_list=[], bSortDescending=False, tagBgEven="#f5f5f5", tagBgOdd="#dcdcff", objLoggerLog=None, **kwargs):
 		self.bSortDescending = bSortDescending
+		self.objLoggerLog = objLoggerLog
+
 		super(clListBoxMultiColumnSort, self).__init__(master=master, **kwargs)
-		
+
+		# Sorting key array. This has to be updated when any new sorting function is added
+		self.arrSortKeys = ["date", "float", "multidecimal", "num", "numcomma", "name", "time"]
+
 		# Row colouring
-		self.tag_configure("tgEvenRow", background=tagBgEven)				
+		self.tag_configure("tgEvenRow", background=tagBgEven)
 		self.tag_configure("tgOddRow", background=tagBgOdd)
 	# End of __init__()
 	
-	def delete(self, *items):
-		super(clListBoxMultiColumnSort, self).delete(*items)
-		
+	def delete(self, *tiID):
+		super(clListBoxMultiColumnSort, self).delete(*tiID)
+
 		# Retag row colours
-		for item in self.get_children():
+		for item in self.get_children(""):
 			index = self.index(item)
 			if index % 2 == 0:
 				self.item(item, tags="tgEvenRow")
@@ -257,10 +383,17 @@ class clListBoxMultiColumnSort(objTTK.Treeview):
 		# End of if
 		return super().heading(column, **kwargs)
 	# End of heading()
-	
+
 	def SortByFunctionName(self, strSortType, strColumnName, reverse):
 		# Form function name
-		strFunction = "".join(["_sort_by_", strSortType])
+		if strSortType in self.arrSortKeys:
+			strFunction = "".join(["_sort_by_", strSortType])
+		else:
+			strFunction = "_sort_by_name"
+			if self.objLoggerLog is not None:
+				self.objLoggerLog("ERROR: <clListBoxMultiColumnSort> : Defaulting to sort by name strSortType:", strSortType)
+			# End of if
+		# End of if
 
 		# Get function object
 		objFunction = getattr(self, strFunction)
@@ -268,44 +401,62 @@ class clListBoxMultiColumnSort(objTTK.Treeview):
 	# End of SortByFunctionName()	
 
 	def _sort(self, column, reverse, data_type, callback):
-		l = [(self.set(k, column), k) for k in self.get_children('')]
-		l.sort(key=lambda t: data_type(t[0]), reverse=reverse)
-		
+		arrValueIDPair = [(self.set(iID, column), iID) for iID in self.get_children("")]
+		arrValueIDPair.sort(key=lambda objValue: data_type(objValue[0]), reverse=reverse)
+
 		# Move and retag row colours
-		for index, (_, k) in enumerate(l):
-			self.move(k, '', index)
-			if index % 2 == 0:
-				self.item(k, tags="tgEvenRow")
+		for iIndex, (_, iID) in enumerate(arrValueIDPair):
+			self.move(iID, '', iIndex)
+			if iIndex % 2 == 0:
+				self.item(iID, tags="tgEvenRow")
 			else:
-				self.item(k, tags="tgOddRow")
+				self.item(iID, tags="tgOddRow")
 			# End of if
 		# End of for loop
 		self.heading(column, command=partial(callback, column, not reverse))
 	# End of _sort()
 
-	def _sort_by_time(self, column, reverse):
-		def _str_to_time(string):
-			if len(string) == 0:
-				string = "12:00am"
-			# End of if
-			return objDateTime.datetime.strptime(string, "%I:%M%p")
-		# End of _str_to_time()
-		
-		self._sort(column, reverse, _str_to_time, self._sort_by_time)
-	# End of _sort_by_time()
-
 	def _sort_by_date(self, column, reverse):
 		def _str_to_date(string):
 			if len(string) == 0:
+				# Very old date
 				string = "1973-10-03"
 			# End of if
 
 			# End of try / except
 			return objDateTime.datetime.strptime(string, "%Y-%m-%d")
 		# End of _str_to_date()
-		
+
 		self._sort(column, reverse, _str_to_date, self._sort_by_date)
 	# End of _sort_by_date()
+
+	def _sort_by_float(self, column, reverse):
+		self._sort(column, reverse, float, self._sort_by_float)
+	# End of _sort_by_float()
+
+	def _sort_by_multidecimal(self, column, reverse):
+		def _multidecimal_to_str(string):
+			arrString = string.split(".")
+			arrNum = []
+			for strString in arrString:
+				strValue = f"{int(strString):02}"
+				arrNum.append(strValue)
+			# End of for loop
+
+			arrNum.append("000000000000000000")
+			strNum = "".join(arrNum)
+
+			if self.objLoggerLog is not None:
+				if len(strNum) > 36:
+					self.objLoggerLog("ERROR: <clListBoxMultiColumnSort> : Number is truncated as it exceeds limit. string:", string)
+				# End of if
+			# End of if
+
+			return int(strNum[:18])
+		# End of _multidecimal_to_str()
+
+		self._sort(column, reverse, _multidecimal_to_str, self._sort_by_multidecimal)
+	# End of _sort_by_multidecimal()
 
 	def _sort_by_name(self, column, reverse):
 		self._sort(column, reverse, str, self._sort_by_name)
@@ -315,30 +466,22 @@ class clListBoxMultiColumnSort(objTTK.Treeview):
 		self._sort(column, reverse, int, self._sort_by_num)
 	# End of _sort_by_num()
 
-	def _sort_by_float(self, column, reverse):
-		self._sort(column, reverse, float, self._sort_by_float)
-	# End of _sort_by_float()
-
-	def _sort_by_multidecimal(self, column, reverse):
-		def _multidecimal_to_str(string):
-			arrString = string.split(".")
-			strNum = ""
-			for iValue in arrString:
-				strValue = f"{int(iValue):02}"
-				strNum = "".join([strNum, str(strValue)])
-			# End of for loop
-			strNum = "".join([strNum, "0000000"])
-			return int(strNum[:8])
-		# End of _multidecimal_to_str()
-
-		self._sort(column, reverse, _multidecimal_to_str, self._sort_by_multidecimal)
-	# End of _sort_by_multidecimal()
-
 	def _sort_by_numcomma(self, column, reverse):
 		def _numcomma_to_num(string):
 			return int(string.replace(",", ""))
 		# End of _numcomma_to_num()
 		
 		self._sort(column, reverse, _numcomma_to_num, self._sort_by_numcomma)
-	# End of _sort_by_numcomma()	
+	# End of _sort_by_numcomma()
+
+	def _sort_by_time(self, column, reverse):
+		def _str_to_time(string):
+			if len(string) == 0:
+				string = "12:00AM"
+			# End of if
+			return objDateTime.datetime.strptime(string, "%I:%M%p")
+		# End of _str_to_time()
+
+		self._sort(column, reverse, _str_to_time, self._sort_by_time)
+	# End of _sort_by_time()
 # End of clListBoxMultiColumnSort
